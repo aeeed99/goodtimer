98b76c4e680263c5bfb1552ef73b50ec92456198
Nick Palenchar <nickpal@nickpalenchar.com>
2021-01-24 18:49:46 -0500

    tests passing for getters and seters

--- lib/latest/time.ts
+++ lib/latest/time.ts
@@ -8,13 +8,74 @@ const { parse } = Timer.prototype;
 class Time {
     _time: Array<Array<number>> = [];
 
-    constructor(time: number | string | Time) {
+    constructor(time: number | string | Time = "0") {
+        if (typeof time === 'number') {
+            // @ts-ignore
+            time = time.toString();
+        }
         if (typeof time === 'string') {
             this._time = parse(time).map(val => val === null ? [0] : [val]);
         }
         else if (typeof time === 'number') {
 
         }
+        else if (time instanceof Time) {
+            // todo
+        }
+        else {
+            throw new TypeError("Can't parse type.");
+        }
+    }
+
+    add(time: number | string | Time): void {
+        const toAdd = time instanceof Time ? time : new Time(time);
+
+        this.milliseconds = this.milliseconds + toAdd.milliseconds;
+        this.seconds = this.seconds + toAdd.seconds;
+        this.minutes = this.minutes + toAdd.minutes;
+        this.hours = this.hours + toAdd.hours;
+        this.days = this.days + toAdd.days;
+        this.years = this.years + toAdd.years;
+    }
+
+    _adjustTime(milliseconds: number) {
+        /** Adjusts time by a number of seconds. Pass negative number to decrement.
+         */
+        const {_adjustAndCarry: aac} = this;
+
+        aac(this._time[0], Infinity,
+            aac(this._time[1], 364,
+                aac(this._time[2], 23,
+                    aac(this._time[3], 59,
+                        aac(this._time[4], 59,
+                            aac(this._time[5], 999, milliseconds))))));
+    }
+
+    _adjustAndCarry(num: number[], resetValue: number, interval: number): number {
+
+        let val: number = num[0] + interval;
+        let carry: number = 0;
+
+        while (val < 0) {
+            val += (resetValue + 1);
+            carry -= 1;
+        }
+        while (val > resetValue) {
+            val -= (resetValue + 1);
+            carry += 1;
+        }
+        num[0] = val;
+        return carry;
+    }
+
+    _getCarryover(num, threshold) {
+        /** Calculates the "carry-over", that is, how many whole units can be divided from a
+         * threshold. This is used to calculate when, say, the seconds unit has more than allowed (60).
+         * In that case, a number of "minutes" should be extracted from it (over the "threshold" of 60).
+         */
+        const carryover = (num - (num % threshold)) / threshold;
+        const remaining = num % threshold;
+        return [carryover, remaining];
     }
 
      _fromMiliseconds(num) {
@@ -23,39 +84,34 @@ class Time {
         if (result[5] < 1000) {
             return result;
         }
-        const seconds = (result[5] - (result[5] % 1000) ) / 1000
-        result[5] %= 1000;
-        result[4] = seconds;
+        [result[4], result[5]] = this._getCarryover(result[5], 1000);
+
 
         if(result[4] < 60) {
             return result;
         }
-        const minutes = (seconds - (seconds % 60)) / 60;
-        result[4] %= 60;
-        result[3] = minutes;
+        [result[3], result[4]] = this._getCarryover(result[4], 60);
+        const minutes = result[3];
 
         if(result[3] < 60) {
             return result
         }
-        const hours = (minutes - (minutes % 60)) / 60;
-        result[2] = hours;
-        result [3] %= 60;
+        [result[2], result[3]] = this._getCarryover(result[3], 60);
+        const hours = result[2];
 
         if(hours < 24) {
             return result;
         }
-        const days = (hours - (hours % 24)) / 24;
-        result[1] = days;
-        result[2] %= 24;
+        [result[1], result[2]] = this._getCarryover(result[2], 24);
+        const days = result[1];
 
         if(result[1] < 365) {
             return result;
         }
-        const years = (days - (days % 365)) / 365;
-        result[0] = years;
-        result[1] %= 365
+        [result[0], result[1]] = this._getCarryover(result[1], 365);
         return result
     }
+
     // [years, days, hours, minutes, seconds, milliseconds]
     //  0      1     2      3        4        5
     get milliseconds() {
@@ -63,6 +119,9 @@ class Time {
     }
     set milliseconds(n) {
         this._time[5][0] = n;
+        if (this._time[5][0] >= 1000) {
+            [this._time[4][0], this._time[5][0]] = this._getCarryover(this._time[5][0], 1000);
+        }
     }
     get ms() {
         /** Alias of .milliseconds. **/
@@ -77,6 +136,9 @@ class Time {
     }
     set seconds(n) {
         this._time[4][0] = n;
+        if (this._time[4][0] >= 60) {
+            [this._time[3][0], this._time[4][0]] = this._getCarryover(this._time[4][0], 60);
+        }
     }
     get secs() {
         /** Alias of this#seconds **/
@@ -90,6 +152,9 @@ class Time {
     }
     set minutes(n) {
         this._time[3][0] = n
+        if (this._time[3][0] >= 60) {
+            [this._time[2][0], this._time[3][0]] = this._getCarryover(this._time[3][0], 60);
+        }
     }
     get mins() {
         /** Alias of .minutes **/
@@ -102,15 +167,26 @@ class Time {
         return this._time[2][0];
     }
     set hours(n) {
-        this._time[1][0] = n;
+        this._time[2][0] = n;
+        if (this._time[2][0] >= 24) {
+            [this._time[1][0], this._time[2][0]] = this._getCarryover(this._time[2][0], 24);
+        }
     }
     get days() {
-        return this._time[0][0];
+        return this._time[1][0];
     }
     set days(n) {
-        this._time[0][0] = n
+        this._time[1][0] = n
+        if (this._time[1][0] >= 365) {
+            [this._time[0][0], this._time[1][0]] = this._getCarryover(this._time[1][0], 365);
+        }
     }
-
+    get years() {
+        return this._time[0][0];
+    }
+    set years(n) {
+        this._time[0][0] = n;
+    }
 }
 
 
